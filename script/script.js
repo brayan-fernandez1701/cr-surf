9d528bcccd8b6a8125280872764014125d4d977a
Brayan Fernandez <bra1701.bf@gmail.com>
2025-08-01 19:01:40 -0600

    add initial files

---
+++ script/script.js
@@ -0,0 +1,23 @@
+// const header = document.getElementById("header")
+// const aNav = document.querySelectorAll(".nav-a")
+// const btnA = document.querySelector(".a-btn")
+
+
+
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY > 50) {
+//         header.classList.add("header");
+//         for (let index = 0; index < aNav.length; index++) {
+//             aNav[index].classList.add("nav-aS");
+//         }
+//         btnA.classList.add("a-btnS")
+
+//     } else {
+//         header.classList.remove("header");
+//         for (let index = 0; index < aNav.length; index++) {
+//             aNav[index].classList.remove("nav-aS");
+//         }
+//         btnA.classList.remove("a-btnS")
+
+//     }
+// })
